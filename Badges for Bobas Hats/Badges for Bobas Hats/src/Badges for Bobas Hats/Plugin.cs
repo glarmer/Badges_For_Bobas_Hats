870c737d6c4d4e4dde4c1ac138239736c4630c90
Lewis <hallrudfordlewis@gmail.com>
2025-09-02 12:08:52 +0100

    Comments

--- Badges for Bobas Hats/Badges for Bobas Hats/src/Badges for Bobas Hats/Plugin.cs
+++ Badges for Bobas Hats/Badges for Bobas Hats/src/Badges for Bobas Hats/Plugin.cs
@@ -20,11 +20,19 @@ public partial class Plugin : BaseUnityPlugin
         BadgeRegistry.Init();
         Logger.LogInfo("Badges for Bobas Hats are loaded!");
         
+        //Patch for Labubu hats
         _harmony.PatchAll(typeof(CharacterOnJumpPatch));
+        
+        //Patch for Toast hat
         _harmony.PatchAll(typeof(CharacterAfflictionsAddStatusPatch));
+        
+        //Patch for Sprout hat
         _harmony.PatchAll(typeof(MagicBeanGrowVineRPCPatch));
+        
+        //Patch for Bear hat
         _harmony.PatchAll(typeof(ActionConsumeRunActionPatch));
         
+        //Patches for Disco hat
         _harmony.PatchAll(typeof(CharacterAnimationsRPCAPlayRemove));
         _harmony.PatchAll(typeof(BugleSFXRPCStartTootPatch));
         _harmony.PatchAll(typeof(BugleSFXRPCEndTootPatch));
